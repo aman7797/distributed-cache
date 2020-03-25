3adc0209ef36e5a3ca36a1f8511af87367e81c61
Aman Lalpuria <Lalpuria_Am@India.XoriantCorp.com>
2020-03-25 22:23:56 +0530

    Redis Spring Boot Integration 1. For String as Datatype 2. For List as Datatype 3. For Set as Datatype

--- redis/src/main/java/com/cache/redis/RedisApplication.java
+++ redis/src/main/java/com/cache/redis/RedisApplication.java
@@ -1,19 +1,13 @@
-package com.cache.redis;
+package com.cache.Redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 
 @SpringBootApplication
 public class RedisApplication {
 
-	@Bean
-	JedisConnectionFactory jedisConnectionFactory() {
-		return new JedisConnectionFactory();
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(RedisApplication.class, args);
 	}
+
 }
